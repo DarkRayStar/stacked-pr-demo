3dc309f5cedb3971d257cc28556fe480170f3fa3
DarkRayStar <dulshanalaha@gmail.com>
2024-10-30 17:48:03 +0530

    Feature 5: Add additional changes to the feature to simulate addressing PR comments
    ghstack-source-id: fe7fb15da92411ae6a19db0a587e11e00752a79f
    Pull Request resolved: https://github.com/DarkRayStar/stacked-pr-demo/pull/9

--- feature5.js
+++ feature5.js
@@ -1,5 +1,7 @@
 // Simulate initial state
 function factorial(n) {
+    // Below console log is added to simulate a change after creating initial PRs with ghstack
+    console.log('Generating factorial of ' + n);
     if (n === 0) {
         return 1;
     } else {
